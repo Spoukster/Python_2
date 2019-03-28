5b720e40e540797ab3a7c19df127c1a8c1f65529
Spoukster <spoukster@gmail.com>
2019-03-28 14:50:50 +0300

    Реализована многопоточность сервера.

--- server/__main__.py
+++ server/__main__.py
@@ -2,17 +2,19 @@ import socket
 import json
 import logging
 import select
+import threading
+import collections
 from datetime import datetime
 
 from routes import resolve
 from protocol import make_400, make_404, validate_request, make_response
-from handlers import handle_client_request
+from handlers import write_response, read_request
 from settings import HOST, PORT
 
 error_handler = logging.FileHandler('error.log')
 error_handler.setLevel(logging.CRITICAL)
 
-responses = []
+requests = collections.deque()
 connections = []
 
 try:
@@ -28,6 +30,7 @@ try:
     sock = socket.socket()
     sock.bind((HOST, PORT))
     sock.listen(5)
+    sock.settimeout(0)
 
     logging.info(f'Server start with host: {HOST} and port: {PORT}')
 
@@ -61,39 +64,27 @@ try:
     #     client.close()
 
     while True:
-        client, address = sock.accept()
-        connections.append(client)
-        logging.info(f'Client detect {address}')
+        try:
+            client, address = sock.accept()
+            connections.append(client)
+            logging.info(f'Client detect {"%s:%s" % address}')
+        except OSError:
+            pass
 
         rlist, wlist, xlist = select.select(connections, connections, [], 0)
 
-        # response_obj = responses.pop() if responses else None
+        for client in rlist:
+            read_thread = threading.Thread(target=read_request, args=(client, requests))
+            read_thread.start()
 
-        for client in connections:
+        if requests:
+            request = requests.popleft()
+            for client in wlist:
+                write_thread = threading.Thread(target=write_response, args=(client, request))
+                write_thread.start()
+                logging.info(f'Response {request} handled and sended to {client.getsockname()}')
 
-            if client in rlist:
 
-                data = client.recv(1024)
-                request = json.loads(data.decode('utf-8'))
-
-                action_name = request.get('action')
-
-                response = handle_client_request(request)
-
-                if response.get('code') == 400:
-                    logging.error(f'Bad request: {action_name} request: {request}')
-
-                if response.get('code') == 200:
-                    responses.append(response)
-
-                response_string = json.dumps(response)
-                client.send(response_string.encode('utf-8'))
-
-            if client in wlist:
-                if responses:
-                    for conn in connections:
-                        response_obj_string = json.dumps(response_obj)
-                        conn.send(response_obj_string.encode('utf-8'))
 
 
 except KeyboardInterrupt:
